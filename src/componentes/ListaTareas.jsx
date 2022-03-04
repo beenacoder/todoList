5f412c93589cd4de13471bd07d09330e836c9dd7
Ariel <cervanteslibch@gmail.com>
2022-03-04 00:34:41 -0300

    Agregue funcion para tareas completadas

--- src/componentes/ListaTareas.jsx
+++ src/componentes/ListaTareas.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import Tarea from './Tarea';
 
-const ListaTareas = ({tareas}) => {
+const ListaTareas = ({tareas,}) => {
+
+    const toggleCompletada = (id) => {
+        console.log('Este es el id', id)
+    }
+
     return (  
         <ul className="lista-tareas">
             {
@@ -11,6 +16,8 @@ const ListaTareas = ({tareas}) => {
                 return <Tarea 
                     key = {tarea.id}
                     tarea = {tarea}
+                    toggleCompletada = {toggleCompletada}
+                    
                 />})
             : <div className='lista-tareas__mensaje'> No hay tareas agregadas </div>
         }
--- src/componentes/Tarea.jsx
+++ src/componentes/Tarea.jsx
@@ -1,25 +1,32 @@
 import React, {useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import  {faCheckSquare, faEdit, faTimes} from '@fortawesome/free-solid-svg-icons';
+import  {faCheckSquare, faEdit, faSquare, faTimes} from '@fortawesome/free-solid-svg-icons';
 
-const Tarea = ({tarea}) => {
+const Tarea = ({tarea, toggleCompletada}) => {
     const [editandoTarea, cambiarEditandoTarea] = useState(false);
     const [nuevaTarea, cambiarNuevaTarea] = useState(tarea.texto);
+    
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        cambiarEditandoTarea(false);
+    }
 
     return (
         <li className='lista-tareas__tarea'>
             <FontAwesomeIcon 
-                icon={faCheckSquare}
+                icon={tarea.completada ? faCheckSquare : faSquare}
                 className="lista-tareas__icono lista-tareas__icono-check"
+                onClick={() => {toggleCompletada(tarea.id)}}
             />
             <div className='lista-tareas__texto'>
                 {editandoTarea ? 
-                <form action="" className='formulario-editar-tarea'>
+                <form action="" className='formulario-editar-tarea' onSubmit={handleSubmit}>
                     <input 
                     type="text"
                     className='formulario-editar-tarea__input'
                     value={nuevaTarea}
+                    onChange={(e) => {cambiarNuevaTarea(e.target.value)}}
                     />
                     <button
                     type="submit"
